0a795e67419d5316f97bf401361370c0e2f03601
levminin-source <levminin@gmail.com>
2026-05-24 21:51:03 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ let store = { comments: {}, acknowledgements: {}, extraChanges: [] };
 let db    = null;
 let auth  = null;
 
-const CONFIGURED = FIREBASE_CONFIG.apiKey !== 'PLACEHOLDER_FIREBASE_API_KEY';
+const CONFIGURED = FIREBASE_CONFIG.apiKey !== 'PLACEHOLDER_FIREBASE_API_KEY' && FIREBASE_CONFIG.apiKey !== 'YOUR_API_KEY' && FIREBASE_CONFIG.apiKey.length > 10;
 
 // ============================================================
 // MARSHALL BRAND ASSETS (SVG)
